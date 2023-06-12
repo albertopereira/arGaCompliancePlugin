df514ce74ca3e7cadd6c2bbc63e1cd9e81532ae0
Ricardo Pinho <ricardodepinho@gmail.com>
2023-06-12 22:14:04 +0100

    Update arGaCompliancePluginConfiguration.class.php
    Remove MIT License reference and add first version author

--- config/arGaCompliancePluginConfiguration.class.php
+++ config/arGaCompliancePluginConfiguration.class.php
@@ -9,7 +9,7 @@
  *
  * arGaCompliancePlugin is distributed WITHOUT ANY WARRANTY; 
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- * See the MIT License for more details <https://opensource.org/license/mit/>.
+ * @author Alberto Pereira <albertopereira@gmail.com>
  */
 
 class arGaCompliancePluginConfiguration extends sfPluginConfiguration
